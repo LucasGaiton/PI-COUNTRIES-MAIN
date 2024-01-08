f13d3dc3324d2349874d22caa7647b8dc7093a46
lucasemanuel <gaitonlucas7@gmail.com>
2024-01-08 13:26:32 -0300

    agora

--- api/src/server.js
+++ api/src/server.js
@@ -6,9 +6,9 @@ const cors = require("cors");
 const server = express();
 
 server.use(cors());
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser());
+// server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+// server.use(bodyParser.json({ limit: '50mb' }));
+// server.use(cookieParser());
 server.use(morgan('dev'));
 
 server.use((req, res, next) => {
