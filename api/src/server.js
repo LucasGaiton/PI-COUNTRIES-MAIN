4aa7a67c8948930494955e8538d470466061b646
lucasemanuel <gaitonlucas7@gmail.com>
2024-01-08 12:51:07 -0300

    arreglo cors front

--- api/src/server.js
+++ api/src/server.js
@@ -5,9 +5,22 @@ const cors = require("cors");
 
 const server = express();
 
-server.use(morgan("dev"));
-server.use(express.json());
-server.use(cors());
+server.use(cors())
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*', 'https://pi-countries-main-front.vercel.app/');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', "apprend,delete,entries,foreach,get,has,keys,set,values,Authorization");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('HTTP/1.1 200 OK')
+  next();
+});
+
+server.use('/', routes);
+
 
 server.use(router);
 
